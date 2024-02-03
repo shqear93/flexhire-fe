f468a1c8fa4dfbdf9e47396548fe67e51a5f7ad6
Khaled AbuShqear <qmax93@gmail.com>
2024-02-04 00:20:38 +0300

    add: add sample GET endpoint

---
+++ src/app/api/current-user/route.js
@@ -0,0 +1,12 @@
+export async function GET() {
+    return Response.json({
+        "data": {
+            "currentUser": {
+                "email": "qmax93@gmail.com",
+                "name": "Khaled Abu Shqear",
+                "id": "dXNlcnMtMTM4NzI=",
+                "avatarUrl": "https://cdn.filestackcontent.com/lgJacvBJQHGQdhAKL11n"
+            }
+        }
+    });
+}
